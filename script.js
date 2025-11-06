59063135bfcdba1ba6d9d3a95f27fc03214155d4
fracpre <124399090+fracpre@users.noreply.github.com>
2025-11-06 17:39:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -119,7 +119,7 @@ document.addEventListener('DOMContentLoaded', () => {
   function initializeVisitorCounter() {
     let totalVisitors = localStorage.getItem('totalVisitorCount');
     if (!totalVisitors) {
-      totalVisitors = 921234;
+      totalVisitors = 4;
       localStorage.setItem('totalVisitorCount', totalVisitors);
     } else {
       totalVisitors = parseInt(totalVisitors);
@@ -669,3 +669,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
   typeWriterStart();
 });
+
